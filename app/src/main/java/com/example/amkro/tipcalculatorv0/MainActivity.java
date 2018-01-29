bdb98193913c54c6b9b9059dc34c60e6e76e1707
Kordell <kct1@pct.edu>
2018-01-28 19:50:25 -0500

    This is v4 of the project i believe

--- app/src/main/java/com/example/amkro/tipcalculatorv0/MainActivity.java
+++ app/src/main/java/com/example/amkro/tipcalculatorv0/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.amkro.tipcalculatorv0;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private tipCalculator tipCalc;
+    public NumberFormat money = NumberFormat.getCurrencyInstance();
+    private EditText billEditText;
+    private EditText tipEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tipCalc = new tipCalculator(1.17f,100.0f);
         setContentView(R.layout.activity_main);
+
+        billEditText = (EditText) findViewById(R.id.amount_bill);
+        tipEditText = (EditText) findViewById(R.id.amount_tip_percent);
+
+        //TextChangeHandler tch = new TextChangeHandler( );
+        //billEditText.addTextChangedListener( tch );
+        //tipEditText.addTextChangedListener( tch );
     }
+
+    public void calculate(View v){
+        Log.w("MainActivity", "v = " + v);
+        EditText billEditText = (EditText) findViewById(R.id.amount_bill);
+        EditText tipEditText = (EditText) findViewById(R.id.amount_tip_percent);
+        String billString = billEditText.getText().toString();
+        String tipString = tipEditText.getText().toString();
+
+        TextView tipTextView = (TextView) findViewById(R.id.amount_tip);
+        TextView totalTextView = (TextView) findViewById(R.id.amount_total);
+        try{
+            float billAmount = Float.parseFloat(billString);
+            int tipPercent = Integer.parseInt(tipString);
+
+            tipCalc.setBill(billAmount);
+            tipCalc.setTip(.01f * tipPercent);
+
+            float tip = tipCalc.tipAmount();
+            float total = tipCalc.totalAmount();
+
+            tipTextView.setText(money.format(tip));
+            totalTextView.setText(money.format(total));
+        } catch (NumberFormatException nfe){
+            //pop up an alert view here
+        }
+    }
+    /*
+    private class TextChangeHandler implements TextWatcher {
+        public void afterTextChanged(Editable e){
+            calculate();
+        }
+        public void  beforeTextChanged(CharSequence s, int start, int count, int after){
+
+    }
+        public void  onTextChanged(CharSequence s, int start, int before, int after){
+
+        }
+    }
+    */
 }
--- app/src/main/java/com/example/amkro/tipcalculatorv0/tipCalculator.java
+++ app/src/main/java/com/example/amkro/tipcalculatorv0/tipCalculator.java
@@ -1,7 +1,7 @@
 package com.example.amkro.tipcalculatorv0;
 
 /**
- * Created by amkro on 1/26/2018.
+ * Created by Kordell on 1/26/2018.
  */
 
 public class tipCalculator {
@@ -16,20 +16,25 @@ public class tipCalculator {
     public float getTip(){
         return tip;
     }
+
     public float getBill(){
         return bill;
     }
+
     public void setTip(float newTip){
-        if(newTip > 0)
+        if (newTip > 0)
             tip = newTip;
     }
+
     public void setBill(float newBill){
-        if(newBill>0)
+        if(newBill > 0)
             bill = newBill;
     }
+
     public float tipAmount(){
         return bill * tip;
     }
+
     public float totalAmount(){
         return bill + tipAmount();
     }
